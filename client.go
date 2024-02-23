ad0d85692f7bdf520b1b8d6bf14918b5bc03d53e
Markus Wennrich <markus.wennrich@f-i-ts.de>
2024-02-23 16:20:55 +0100

    Refactor metrics and add connection reset tracking

--- client.go
+++ client.go
@@ -1,50 +1,89 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log/slog"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-type metrics struct {
-	totalDuration *prometheus.HistogramVec
-	totalErrors   *prometheus.CounterVec
-	httpClient    http.Client
+type connDetails struct {
+	addr net.Addr
 }
 
-func NewMetrics(reg prometheus.Registerer) *metrics {
-	buckets := []float64{.0005, .001, .005, .01, .025, .05, .1, .25, .5}
+type metrics struct {
+	totalDuration         *prometheus.HistogramVec
+	totalErrors           *prometheus.CounterVec
+	totalConnectionResets *prometheus.CounterVec
+	httpClients           map[string]*http.Client
+	connDetails           map[string]*connDetails
+	peers                 []string
+}
+
+func NewMetrics(reg prometheus.Registerer, peers []string) *metrics {
+	buckets := []float64{.005, .01, .015, .02, .025, .05, .1, .25, .5, 1}
+
+	httpClients := map[string]*http.Client{}
+	cds := map[string]*connDetails{}
+	for _, p := range peers {
+		cds[p] = &connDetails{}
+
+		httpClients[p] = &http.Client{
+			Transport: &http.Transport{
+				MaxIdleConnsPerHost: 10,
+				DialContext:         cds[p].DialContext,
+			},
+			Timeout: 30 * time.Second,
+		}
+	}
+
 	m := &metrics{
 		totalDuration: prometheus.NewHistogramVec(prometheus.HistogramOpts{
 			Name:    "latency_exporter_seconds_total",
 			Help:    "Histogram of total duration.",
 			Buckets: buckets,
 		}, []string{"peer"}),
+
 		totalErrors: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "latency_exporter_errors_total",
 			Help: "Number of total errors",
 		}, []string{"peer"}),
-		httpClient: http.Client{
-			Transport: &http.Transport{
-				MaxIdleConnsPerHost: 10,
-			},
-			Timeout: 30 * time.Second,
-		},
+
+		totalConnectionResets: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: "latency_exporter_connection_resets_total",
+			Help: "Number of total connection resets",
+		}, []string{"peer"}),
+
+		httpClients: httpClients,
+		connDetails: cds,
+		peers:       peers,
 	}
+
 	reg.MustRegister(m.totalDuration)
 	reg.MustRegister(m.totalErrors)
+	reg.MustRegister(m.totalConnectionResets)
+
 	return m
 }
-func (m *metrics) run(peers []string) {
+func (m *metrics) run() {
+	localAdresses := map[string]string{}
+
 	time.Sleep(5 * time.Second)
 
 	for {
-		for _, peer := range peers {
-			err := m.measureRequest(peer)
+		for _, peer := range m.peers {
+			la, err := m.measureRequest(peer)
+			if _, ok := localAdresses[peer]; !ok {
+				localAdresses[peer] = la
+			} else if localAdresses[peer] != la {
+				m.totalConnectionResets.WithLabelValues(peer).Inc()
+				localAdresses[peer] = la
+			}
 			if err != nil {
 				m.totalErrors.WithLabelValues(peer).Inc()
 				slog.Error("error during measurement", "error", err)
@@ -54,30 +93,49 @@ func (m *metrics) run(peers []string) {
 	}
 }
 
-func (m *metrics) measureRequest(peer string) error {
+func (m *metrics) measureRequest(peer string) (string, error) {
 
 	timeStart := time.Now()
 	req, err := http.NewRequest("GET", "http://"+peer+"/ping", nil)
 	if err != nil {
-		return fmt.Errorf("error creating request: %w", err)
+		return "", fmt.Errorf("error creating request: %w", err)
 	}
 	req.Header.Set("Connection", "keep-alive")
 
-	res, err := m.httpClient.Do(req)
+	res, err := m.httpClients[peer].Do(req)
 	timeDone := time.Now()
 
 	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("error response: %d", res.StatusCode)
+		return "", fmt.Errorf("error response: %d", res.StatusCode)
 	}
 	if err != nil {
-		return fmt.Errorf("error sending request: %w", err)
+		return "", fmt.Errorf("error sending request: %w", err)
 	}
 	_, err = io.ReadAll(res.Body)
 	if err != nil {
-		return fmt.Errorf("error reading body: %w", err)
+		return "", fmt.Errorf("error reading body: %w", err)
 	}
 	res.Body.Close()
 	m.totalDuration.WithLabelValues(peer).Observe(float64(timeDone.Sub(timeStart).Seconds()))
 
-	return nil
+	// fmt.Println("Local address:", m.connDetails[peer].addr.String())
+
+	return m.connDetails[peer].addr.String(), nil
+}
+
+func (m *metrics) init() {
+	for _, peer := range m.peers {
+		m.totalErrors.WithLabelValues(peer).Add(0)
+		m.totalConnectionResets.WithLabelValues(peer).Add(0)
+	}
+}
+func (cd *connDetails) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	conn, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	cd.addr = conn.LocalAddr()
+
+	return conn, nil
 }
--- main.go
+++ main.go
@@ -14,20 +14,21 @@ import (
 )
 
 func main() {
+	peerFlag := flag.String("peers", "127.0.0.1:9080", "peers")
+	flag.Parse()
+	peers := strings.Split(*peerFlag, ",")
 
 	reg := prometheus.NewRegistry()
 
 	// Create new metrics and register them using the custom registry.
-	m := NewMetrics(reg)
+	m := NewMetrics(reg, peers)
+	m.init()
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong\n"))
+		w.Write([]byte("pong"))
 	})
 
-	peerFlag := flag.String("peers", "127.0.0.1:9080", "peers")
-	flag.Parse()
-	peers := strings.Split(*peerFlag, ",")
-	go m.run(peers)
+	go m.run()
 	http.Handle("/ping", handler)
 	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 	slog.Info("Beginning to serve on port :9080")
